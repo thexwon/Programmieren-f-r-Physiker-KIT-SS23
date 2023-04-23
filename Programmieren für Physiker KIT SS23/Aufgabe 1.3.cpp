2afa64b2fb22e1cdc5187acabd36197a7ad9fd02
thexwon <felix.schwarz12@gmail.com>
2023-04-23 16:23:54 +0200

    Aufgabe 1.3

---
+++ Programmieren für Physiker KIT SS23/Aufgabe 1.3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    float x;
+    cout << "Enter float x: ";
+    cin >> x;
+    cout << "e^x is " << exp(x);
+    return 0;
+}
